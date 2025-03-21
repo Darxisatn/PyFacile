d5ce5ee8f5586de64dbc8bfb4f35d353d5edb00d
Darxisatn <152013654+Darxisatn@users.noreply.github.com>
2025-03-21 16:50:53 +0100

    Create TP2.py

---
+++ TPs/TP2.py
@@ -0,0 +1,114 @@
+#TP2 - Manipulation de listes
+
+# Etape 1 : supprime "pass" pour coder la fonction
+# Etape 2 : supprime les """ pour pouvoir effectuer les tests
+
+# EXO 1
+# Objectif : Créer une liste contenant les n premiers entiers naturels
+def list1(n):
+    pass
+
+#Test 
+"""
+print("list1:\n   attendu: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]\n   reçu:   "+str(list1(10)))
+"""
+
+# EXO 2
+# Objectif : Créer une liste contenant les entiers naturels de n à m
+def list2(n, m):
+    pass
+
+#Test 
+"""
+print("list2:\n   attendu: [10, 11, 12, 13, 14, 15, 16, 17, 18, 19]\n   reçu:   "+str(list2(10, 19)))
+"""
+
+# EXO 3
+# Objectif : Créer une liste contenant les n premiers entiers pairs
+def list3(n):
+    pass
+
+#Test
+"""
+print("list3:\n   attendu: [0, 2, 4, 6, 8, 10, 12, 14, 16, 18]\n   reçu:   "+str(list3(10)))
+"""
+
+# EXO 4
+# Objectif : Créer une liste contenant les n premiers entiers impairs
+def list_impairs(n):
+    pass
+
+#Test
+"""
+print("list_impairs:\n   attendu: [1, 3, 5, 7, 9]\n   reçu:   "+str(list_impairs(5)))
+"""
+
+# EXO 5
+# Objectif : Multiplier tous les éléments d'une liste par x
+def mul_list(liste, x):
+    pass
+
+#Test
+"""
+print("mul_list:\n   attendu: [-2, 0, 2, 4]\n   reçu:   "+str(mul_list([-1, 0, 1, 2], 2)))
+"""
+
+# EXO 6
+# Objectif : Calculer la somme de tous les éléments d'une liste
+def somme_liste(liste):
+    pass
+
+#Test
+"""
+print("somme_liste:\n   attendu: 15\n   reçu:   "+str(somme_liste([1, 2, 3, 4, 5])))
+"""
+
+# EXO 7
+# Objectif : Calculer la moyenne des éléments d'une liste
+def moyenne_liste(liste):
+    pass
+
+#Test
+"""
+print("moyenne_liste:\n   attendu: 3.0\n   reçu:   "+str(moyenne_liste([1, 2, 3, 4, 5])))
+"""
+
+# EXO 8
+# Objectif : Trouver le maximum dans une liste sans utiliser la fonction max()
+def maximum_liste(liste):
+    pass
+
+#Test
+"""
+print("maximum_liste:\n   attendu: 5\n   reçu:   "+str(maximum_liste([1, 5, 3, 2, 4])))
+"""
+
+# EXO 9
+# Objectif : Inverser une liste sans utiliser la fonction reverse()
+def inverser_liste(liste):
+    pass
+
+#Test
+"""
+print("inverser_liste:\n   attendu: [5, 4, 3, 2, 1]\n   reçu:   "+str(inverser_liste([1, 2, 3, 4, 5])))
+"""
+
+# EXO 10
+# Objectif : Compter les occurrences d'un élément dans une liste
+def compter_occurrences(liste, element):
+    pass
+
+#Test
+"""
+print("compter_occurrences:\n   attendu: 3\n   reçu:   "+str(compter_occurrences([1, 2, 2, 3, 2, 4], 2)))
+"""
+
+# EXO 11
+# Objectif : Fusionner deux listes alternativement
+def fusionner_alternativement(liste1, liste2):
+    pass
+
+#Test
+"""
+print("fusionner_alternativement:\n   attendu: [1, 'a', 2, 'b', 3, 'c']\n   reçu:   "+str(fusionner_alternativement([1, 2, 3], ['a', 'b', 'c'])))
+"""
